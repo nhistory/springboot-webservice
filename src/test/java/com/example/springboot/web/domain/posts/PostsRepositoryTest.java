d02d91121548833dfe0421020c4df99c39183212
nhistory <nhistory@naver.com>
2022-03-27 23:05:19 -0300

    [#3] JPA/Posts class, repository and test class added

---
+++ src/test/java/com/example/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.example.springboot.web.domain.posts;
+
+import com.example.springboot.domain.posts.Posts;
+import com.example.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void posting_call(){
+        //given
+        String title = "test posting";
+        String content = "test content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("sehwan.lee.dev@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
